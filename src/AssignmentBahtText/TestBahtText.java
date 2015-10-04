32745f7bca69829ff54b25c4e9cb6d87bf8cb5e8
INT675 <INT675@172.16.25.13>
2015-10-04 15:47:57 +0700

    WEEK10

---
+++ src/AssignmentBahtText/TestBahtText.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package AssignmentBahtText;
+
+/**
+ *
+ * @author INT675
+ */
+public class TestBahtText {
+//    @BeforeClass
+//    public static void initialize()
+//        throws Exception
+//    {
+//    }
+//
+//    @AfterClass
+//    public static void destroy()
+//        throws Exception
+//    {
+//    }
+//
+//    @Before
+//    public void setup()
+//        throws Exception
+//    {
+//    }
+//
+//    @After
+//    public void tearDown()
+//        throws Exception
+//    {
+//    }
+//
+//    @Test
+//    public void testBahtTextClass()
+//        throws Exception
+//    {
+//        assertEquals("ลบหนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",
+//            new BahtText(-1234.5463).toString());
+//        assertEquals("หนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",
+//            new BahtText(1234.5463).toString());
+//        assertEquals("ลบหนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",
+//            new BahtText("-1234.5463").toString());
+//        assertEquals("หนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",
+//            new BahtText("1234.5463").toString());
+//        assertEquals("ลบหนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",
+//            new BahtText(new BigDecimal(-1234.5463)).toString());
+//        assertEquals("หนึ่งร้อยเอ็ดบาทถ้วน", new BahtText(101).toString());
+//        assertEquals("หนึ่งร้อยเอ็ดบาทแปดสิบสตางค์",
+//            new BahtText(101.8f).toString());
+//        assertEquals("เก้าล้านสองแสนสองหมื่นสามพันสามร้อยเจ็ดสิบสองล้านสามหมื่นหกพันแปดร้อยห้าสิบสี่ล้านเจ็ดแสนเจ็ดหมื่นห้าพันแปดร้อยเจ็ดบาทถ้วน",
+//            new BahtText(Long.MAX_VALUE).toString());
+//        assertEquals("สองพันหนึ่งร้อยสี่สิบเจ็ดล้านสี่แสนแปดหมื่นสามพันหกร้อยสี่สิบเจ็ดบาทถ้วน",
+//            new BahtText(Integer.MAX_VALUE).toString());
+//    }
+//
+//    @Test
+//    public void testEquality()
+//        throws Exception
+//    {
+//        assertTrue(new BahtText(-1234).equals(new BahtText("-1234")));
+//        assertTrue(new BahtText(127).equals(new BahtText((byte) 127)));
+//        assertTrue(new BahtText("127").equals(new BahtText((byte) 127)));
+//        assertTrue(new BahtText("127").equals(new BahtText((short) 127)));
+//        assertTrue(new BahtText("127").equals(new BahtText((long) 127)));
+//        assertTrue(new BahtText("127").equals(new BahtText((double) 127)));
+//    }
+//public static void main(String[] args) {
+//        //BahtText bt = new BahtText();
+//        assertEquals("ลบหนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",
+//            new BahtText(-1234.5463).toString());
+//       assertEquals("ลบหนึ่งพันสองร้อยสามสิบสี่บาทห้าสิบห้าสตางค์",new BahtText(-1234.5463).toString());
+//
+//    private static String assertEquals(String text, String num) {
+//        return "สองร้อย";
+//    }
+}
